81099de9e3ab7fcf08fab69128d8e029512c03a9
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-05-08 14:03:21 +0530

    fix: join section responsiveness

--- src/components/ui/Join.tsx
+++ src/components/ui/Join.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 export default function Join() {
   return (
     <div className='flex flex-col items-center justify-center my-6'>
-      <div className='flex justify-center items-center w-[50%] border-neutral-800'>
+      <div className='flex flex-col lg:flex-row justify-center items-center lg:w-[50%] border-neutral-800'>
         <input
           type='text'
           title='email'
           placeholder='Enter your email'
-          className='pl-4 bg-black border-neutral-600 border-2 py-4 px-8 rounded-t-lg rounded-b-lg rounded-l-lg rounded-tr-none rounded-br-none'
+          className='pl-4 bg-black border-neutral-600 border-2 py-4 px-8 rounded-t-lg rounded-b-lg lg:rounded-l-lg lg:rounded-r-none lg:rounded-tl-lg lg:rounded-bl-lg lg:rounded-tr-none lg:rounded-br-none lg:mr-0 mb-2 lg:mb-0'
         />
-        <button className='text-center py-3 px-8 border-sky-400 
+        <button className='text-center py-3 px-6 lg:px-8 border-sky-400 
         border-t-2 border-b-2 border-r-2 rounded-t-lg rounded-b-lg 
-        rounded-r-lg rounded-tl-none rounded-bl-none bg-sky-400 font-bold text-2xl'>
+        rounded-r-lg lg:rounded-tl-none lg:rounded-bl-none bg-sky-400 font-bold text-2xl'>
           Join
         </button>
       </div>
